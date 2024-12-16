1815bf501e5253a87c69e14e3ac25bd11b16ad7d
mm620 <mm6205@duck.com>
2024-12-16 22:46:12 +0100

    Test c

--- tests/testy.py
+++ tests/testy.py
@@ -12,25 +12,26 @@ def test_a_add_to_cart():
     time.sleep(1)
     
     category1 = driver.find_element(By.ID, "category-3")
-    add_products_in_category(category1)
+    add_products_in_category(category1, 5)
     category2 = driver.find_element(By.ID, "category-6")
-    add_products_in_category(category2)
+    add_products_in_category(category2, 5)
 
-    # verify cart contents
-    cart = driver.find_element(By.CLASS_NAME, "cart-preview")
-    assert "7" in cart.text  # FIXME change to 10 when we have all products loaded
+    verify_cart_contents("7")  # FIXME change to 10 when we have all products loaded
     print("Test A passed!")
     driver.delete_all_cookies()
 
+def verify_cart_contents(contents):
+    cart = driver.find_element(By.CLASS_NAME, "cart-preview")
+    assert contents in cart.text
 
-def add_products_in_category(category):
+def add_products_in_category(category, max_prods):
     category.click()
     time.sleep(1)
     
     while True:
         products = driver.find_elements(By.CLASS_NAME, "thumbnail-top")
 
-        for index in range(min(5, len(products))): # max 5 products per category
+        for index in range(min(max_prods, len(products))): # max 5 products per category
             # re-fetch products to avoid stale refs
             products = driver.find_elements(By.CLASS_NAME, "thumbnail-top")
             product = products[index]
@@ -67,12 +68,36 @@ def test_b_search_and_add_to_cart():
     # verify cart contents
     driver.refresh()
     time.sleep(2)
-    cart = driver.find_element(By.CLASS_NAME, "cart-preview")
-    assert "1" in cart.text
+    verify_cart_contents("1")
 
     print("Test B passed!")
     driver.delete_all_cookies()
 
+def test_c_remove_from_cart():
+    driver.get("http://localhost:8080/pl/")
+    time.sleep(1)
+    
+    category = driver.find_element(By.ID, "category-6")
+    add_products_in_category(category, 3)
+    
+    # verify cart contents
+    verify_cart_contents("3")
+    cart = driver.find_element(By.CLASS_NAME, "cart-preview")
+    cart.click()
+    time.sleep(2)
+
+    buttons = driver.find_elements(By.CLASS_NAME, "remove-from-cart")
+    for index in range(2, -1, -1):
+        # re-fetch to avoid stale refs
+        button = buttons[index]
+        button.click()
+        time.sleep(1)
+
+    time.sleep(10)
+    verify_cart_contents("0")
+    print("Test C passed!")
+    driver.delete_all_cookies()
 
 #test_a_add_to_cart()
-test_b_search_and_add_to_cart()
+#test_b_search_and_add_to_cart()
+test_c_remove_from_cart()
